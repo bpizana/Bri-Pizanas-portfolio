5a65ae8730a9abd9b15495bc356872f3e2b2b56c
Brianna <bripiz@gmail.com>
2018-02-23 15:29:55 -0800

    Mobile css - slideshow js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,27 @@ var animateHTML = function () {
   }
 }
 animateHTML().init()
+
+
+
+// Image slideshow
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mobile-screen");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
